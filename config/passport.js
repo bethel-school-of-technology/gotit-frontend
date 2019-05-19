f031a165d0c0ac29961ade0b12073104bc37e5be
radrivera <43865296+radrivera@users.noreply.github.com>
2019-05-18 21:33:02 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,21 @@
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const LocalStrategy = require('passport-local').Strategy;
 const models = require('../models');
+const keys = require('./keys');
+
+
+passport.use(
+  new GoogleStrategy({
+      // options for google strategy
+      callbackURL:'/auth/google/redirect',
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret
+      }, () => {
+      // passport callback function
+  })
+);
+
 
 
 passport.use(
